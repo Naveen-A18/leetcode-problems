af8a6eabe03ec281682aa249d465362a96640d0e
Naveen A <2006naveen18@gmail.com>
2026-02-16 19:06:07 +0530

    Add function to find median of two sorted arrays

--- 4.c
+++ 4.c
@@ -1,1 +1,9 @@
-4.c
+#include <limits.h>
+
+double findMedianSortedArrays(int* nums1, int m, int* nums2, int n) 
+{
+    // Ensure nums1 is smaller array
+    if (m > n) 
+        return findMedianSortedArrays(nums2, n, nums1, m);
+
+    int low = 0, high = m;
