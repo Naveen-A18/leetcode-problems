3499d41ba6fd776dcf95264493c35f624fcea039
Naveen A <2006naveen18@gmail.com>
2026-02-22 15:29:15 +0530

    Update 6.c

--- 6.c
+++ 6.c
@@ -1,1 +1,31 @@
- 
+ #include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+char* convert(char* s, int numRows) {
+    int len = strlen(s);
+    
+    if (numRows == 1 || numRows >= len)
+        return s;
+
+    // Allocate memory for result
+    char* result = (char*)malloc((len + 1) * sizeof(char));
+    
+    int cycle = 2 * numRows - 2;
+    int index = 0;
+
+    for (int i = 0; i < numRows; i++) {
+        for (int j = i; j < len; j += cycle) {
+            
+            // Vertical element
+            result[index++] = s[j];
+
+            // Diagonal element (avoid first & last row)
+            int diagonal = j + cycle - 2 * i;
+            if (i != 0 && i != numRows - 1 && diagonal < len) {
+                result[index++] = s[diagonal];
+            }
+        }
+    }
+
+    
